fb7e9be9cfec99547833de6fda7b9f6dd3b7dbdc
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-23 17:59:15 -0300

    Sistema de filtro de comentários

--- classe-01/index.js
+++ classe-01/index.js
@@ -1,1 +1,9 @@
-const comentario = "Esse COVID é muito perigoso!";
+const comentario = "Esse covid é muito perigoso!";
+
+const resultado = comentario.toLowerCase();
+
+if (resultado.includes("covid") || resultado.includes("pandemia")) {
+    console.log("Comentário bloqueado por conter palavras proibidas");
+} else {
+    console.log("Comentário autorizado.");
+}
