51f1b20adbbc729615db696a71a4964b26c5bda5
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-24 21:51:30 -0300

    URL amigável

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,13 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+
+let validacao = "";
+let textoFormatado = texto;
+
+textoFormatado = textoFormatado.toLowerCase();
+
+while (validacao != textoFormatado) {
+    validacao = textoFormatado;
+    textoFormatado = textoFormatado.replace(" ", "-");
+}
+
+console.log(textoFormatado);
