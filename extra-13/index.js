d335fbbd1d1f75d09d0da368e23407dcb7d05ec5
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-28 15:15:14 -0300

    Validação de arquivos

--- extra-13/index.js
+++ extra-13/index.js
@@ -1,1 +1,14 @@
 const nomeArquivo = 'Foto da Familia.pdf';
+
+function validarArquivo(arquivo) {
+    const arquivosValidos = ['jpg', 'jpeg', 'gif', 'png'];
+    const extensao = arquivo.substr(arquivo.lastIndexOf('.') + 1);
+
+    if (arquivosValidos.includes(extensao)) {
+        console.log('Arquivo válido!');
+    } else {
+        console.log('Arquivo inválido!');
+    }
+}
+
+validarArquivo(nomeArquivo);
