be56f719593cbe4ef4a43087f2705e9992ee7774
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-28 14:41:37 -0300

    Grupos de carros

--- extra-11/index.js
+++ extra-11/index.js
@@ -1,2 +1,8 @@
 const nomes = ['Ford Ká', 'Ranger', 'Hilux', 'Corola', 'Fusca', 'Chevete', 'Brasilia'];
 const posicao = 3;
+
+function grupoDeCarros(arrayCarros, posicaoInicial) {
+    console.log(arrayCarros.splice(posicaoInicial, 3));
+}
+
+grupoDeCarros(nomes, posicao);
