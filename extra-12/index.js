e5ce58d4cba88c18a07bdf1f237da2ca63a84ed3
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-28 14:45:50 -0300

    Gerador de Nickname

--- extra-12/index.js
+++ extra-12/index.js
@@ -1,2 +1,9 @@
 const nomes = ['Juninho', 'Léo', 'Guido', 'Dina', 'Vitinho', 'Nanda'];
 const tamanhoDoGrupo = 4;
+
+function gerarNickname(nome) {
+    const nickname = nome.trim().toLowerCase().substr(0, 13).split(' ').join('');
+    console.log(`@${nickname}`);
+}
+
+gerarNickname(nomes[1]);
