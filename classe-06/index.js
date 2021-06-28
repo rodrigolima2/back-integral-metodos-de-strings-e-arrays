546edc70360db9a223425bd1985d0419b6382f44
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-28 10:02:59 -0300

    Teste suas habilidades

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,1 +1,11 @@
 const frutas = ['Banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+
+let frutasReverso = frutas.reverse();
+
+frutasReverso = frutasReverso.join(", ");
+
+frutas.splice(0, 1);
+frutas.splice(frutas.lastIndexOf(), 1, "Melão");
+
+console.log(frutasReverso);
+console.log(frutas);
