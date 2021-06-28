97d2ca0989dd646e9ce3969a53d32e894dc49ab5
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-28 14:34:40 -0300

    Validação de e-mail

--- extra-08/index.js
+++ extra-08/index.js
@@ -1,1 +1,41 @@
 const email = "aluno@cubos.academy";
+
+function validarEmail(email) {
+    let valido = true;
+
+    if (email.indexOf("@") < 0) {
+        valido = false;
+    }
+
+    if (email.indexOf(".") < 0) {
+        valido = false;
+    }
+
+    if (email.indexOf(".") === 0) {
+        valido = false;
+    }
+
+    if (email.lastIndexOf(".") === email.length - 1) {
+        valido = false;
+    }
+
+    if (valido) {
+        console.log("E-mail válido");
+    } else {
+        console.log("E-mail inválido");
+    }
+}
+
+validarEmail("jose@cubos.academy");
+validarEmail("jose@cubos.academy.br");
+validarEmail("jose.messias@cubos.academy");
+validarEmail("jose.messias@cubos.io");
+validarEmail("jose@cubos");
+validarEmail("jose.messias@cubos");
+validarEmail("jose.messias@.");
+validarEmail("jose.@cubos");
+validarEmail(".@");
+validarEmail("@.");
+validarEmail("jose.messias@cubos.");
+validarEmail(".messias@cubos.");
+validarEmail(".messias@cubos");
